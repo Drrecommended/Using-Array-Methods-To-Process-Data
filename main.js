a638fc4916eeebd83e43e9f1c0f9d00c72dc4b17
Tyler French <tylerfrench89@gmail.com>
2020-02-27 20:08:44 -0800

    array method homework 2

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ var itemTotal = 0
 
 items.forEach(function (item, index) {
     itemTotal += item.price
-})
+}) 
 
 var avg = itemTotal / items.length
 
 var theAnswer1 = document.querySelector("#answer1")
-theAnswer1.innerHTML = avg.toFixed(2)
+theAnswer1.innerHTML = `<p>The average price is ${avg.toFixed(2)}.`
+
+// var itemTotal = items.reduce(function (a,b) {
+//     return a + b.price
+// }, 0)
+
 
 ////////////////////////////////////////////////////
 
@@ -58,3 +63,24 @@ theAnswer4.innerHTML = title2
 
 ////////////////////////////////////////////////////
 
+// var eightOrMore = items.filter(function(product) {
+//     return product.materials,length >= 8
+// })
+
+// var html2 = eightOrMore.map(function (item) {
+//     return `
+//     <p>${item.title} has ${item.materials.length}</p>
+
+//     `
+// }).join('')
+
+// theAnswer4 = document.querySelector("#answer5")
+
+
+/////////////////////////////////////////////////////
+
+var madeBySellers = items.filter(function (product) {
+    return product.who_made === 'i_did'
+})
+
+document.querySelector('#answer6').innerHTML = madeBySellers.length
